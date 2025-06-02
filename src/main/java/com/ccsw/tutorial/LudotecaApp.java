a953f5a0044979f9061f338a4281409c25fbcafc
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-02 08:55:30 +0200

    feat: Crear clase LudotecaApp para poder ejecutar el programa

---
+++ src/main/java/com/ccsw/tutorial/LudotecaApp.java
@@ -0,0 +1,12 @@
+package com.ccsw.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudotecaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudotecaApp.class, args);
+    }
+}
