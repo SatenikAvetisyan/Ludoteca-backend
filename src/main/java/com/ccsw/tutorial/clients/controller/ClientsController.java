8b00f711675546248d4e453a28a915db4e9cb057
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-02 11:28:19 +0200

    feat: Crear método para actualizar y crear un cliente

--- src/main/java/com/ccsw/tutorial/clients/controller/ClientsController.java
+++ src/main/java/com/ccsw/tutorial/clients/controller/ClientsController.java
@@ -3,10 +3,7 @@ package com.ccsw.tutorial.clients.controller;
 import com.ccsw.tutorial.clients.model.ClientsDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,5 +27,27 @@ public class ClientsController {
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<ClientsDto> findAll() {
         return new ArrayList<ClientsDto>(this.clientes.values());
+    }
+
+    /**
+     * Método para craer o actualizar una categoria
+     *
+     * @param id PK de la entidad
+     * @param dto datos de la entidad
+     */
+    @Operation(summary = "Save or Update", description = "Method thats saves or update a client")
+    @RequestMapping(path = { "", "/{id}" }, method = RequestMethod.PUT)
+    public void save(@PathVariable(name = "id", required = false) Long id, @RequestBody ClientsDto dto) {
+
+        ClientsDto client;
+
+        if (id == null) {
+            client = new ClientsDto();
+            client.setId(this.SEQUENCE++);
+            this.clientes.put(client.getId(), client);
+        } else {
+            client = this.clientes.get(id);
+        }
+        client.setName(dto.getName());
     }
 }
