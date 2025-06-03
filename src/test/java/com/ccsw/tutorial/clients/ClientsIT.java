4db7eb40203dea74baa7cc4019b139b13810e5f5
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-03 11:04:14 +0200

    feat: Test para probar si el método get nos devuelve 3 resultados

---
+++ src/test/java/com/ccsw/tutorial/clients/ClientsIT.java
@@ -0,0 +1,42 @@
+package com.ccsw.tutorial.clients;
+
+import com.ccsw.tutorial.clients.model.ClientsDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ClientsIT {
+    public static final String LOCALHOST = "http://localhost:";
+    public static final String SERVICE_PATH = "/category";
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    ParameterizedTypeReference<List<ClientsDto>> responseType = new ParameterizedTypeReference<List<ClientsDto>>() {
+    };
+
+    @Test
+    public void findAllShouldReturnAllCategories() {
+        ResponseEntity<List<ClientsDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.GET, null, responseType);
+
+        assertNotNull(response);
+        assertEquals(3, response.getBody().size());
+    }
+
+}
