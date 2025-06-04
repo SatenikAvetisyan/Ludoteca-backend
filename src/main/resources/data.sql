dbe1903bced04f9fab01768b69b34d066c68ef43
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-04 15:39:25 +0200

    feat: añadir datos para categorias

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO clients (name) VALUES ('Alvaro');
 INSERT INTO clients (name) VALUES ('Anna');
 INSERT INTO clients (name) VALUES ('Oscar');
+
+INSERT INTO category(name) VALUES ('Eurogames');
+INSERT INTO category(name) VALUES ('Ameritrash');
+INSERT INTO category(name) VALUES ('Familiar');
