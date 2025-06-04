39b41a51c0ae9a571208d3df6cfd1b24f832721f
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-04 14:52:49 +0200

    feat: servicios añadidos para acceder al repositorio

---
+++ src/main/java/com/ccsw/tutorial/categories/services/CategoryServiceImpl.java
@@ -0,0 +1,60 @@
+package com.ccsw.tutorial.categories.services;
+
+import com.ccsw.tutorial.categories.model.Category;
+import com.ccsw.tutorial.categories.model.CategoryDto;
+import com.ccsw.tutorial.categories.repository.CategoryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Category> findAll() {
+
+        return (List<Category>) this.categoryRepository.findAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void save(Long id, CategoryDto dto) {
+
+        Category category;
+
+        if (id == null) {
+            category = new Category();
+        } else {
+            category = this.categoryRepository.findById(id).orElse(null);
+        }
+
+        category.setName(dto.getName());
+
+        this.categoryRepository.save(category);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(Long id) throws Exception {
+
+        if (this.categoryRepository.findById(id).orElse(null) == null) {
+            throw new Exception("Not exists");
+        }
+
+        this.categoryRepository.deleteById(id);
+    }
+
+}
