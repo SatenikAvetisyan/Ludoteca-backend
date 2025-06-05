8cf412b1274f8a9902946494ea4dd620933302ad
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-05 12:34:24 +0200

    feat: Añadir un método al que pasandole un objeto de tipo Pageable nos devuelve una Page.

--- src/main/java/com/ccsw/tutorial/author/repository/AuthorRepository.java
+++ src/main/java/com/ccsw/tutorial/author/repository/AuthorRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * @author ccsw
- *
- */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+    /**
+     * Método para recuperar un listado paginado de {@link Author}
+     *
+     * @param pageable pageable
+     * @return {@link Page} de {@link Author}
+     */
+    Page<Author> findAll(Pageable pageable);
 
 }
