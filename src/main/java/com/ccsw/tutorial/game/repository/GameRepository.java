d69fb59f21452736727e6c802568233771878485
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-10 12:04:03 +0200

    feat: añadir sobre escritura del método findAll

--- src/main/java/com/ccsw/tutorial/game/repository/GameRepository.java
+++ src/main/java/com/ccsw/tutorial/game/repository/GameRepository.java
@@ -1,9 +1,15 @@
 package com.ccsw.tutorial.game.repository;
 
 import com.ccsw.tutorial.game.model.Game;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GameRepository extends CrudRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+import java.util.List;
 
+public interface GameRepository extends CrudRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+    @Override
+    @EntityGraph(attributePaths = { "category", "author" })
+    List<Game> findAll(Specification<Game> spec);
 }
