1c06857d21c4bb6ca8168fd5fd049aabfaccaad2
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-10 12:56:12 +0200

    feat: craer LoanDto para intercambiar datos entre capas

---
+++ src/main/java/com/ccsw/tutorial/loans/model/LoanDto.java
@@ -0,0 +1,100 @@
+package com.ccsw.tutorial.loans.model;
+
+import com.ccsw.tutorial.categories.model.CategoryDto;
+import com.ccsw.tutorial.game.model.GameDto;
+
+import java.time.LocalDate;
+
+public class LoanDto {
+
+    private Long id;
+
+    private LocalDate loanDate;
+
+    private LocalDate returnDate;
+
+    private CategoryDto category;
+
+    private GameDto game;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+
+        return this.id;
+    }
+
+    /**
+     * @param id new value of {@link #getId}.
+     */
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    /**
+     * @return loanDate
+     */
+    public LocalDate getLoanDate() {
+
+        return this.loanDate;
+    }
+
+    /**
+     * @param loanDate new value of {@link #getLoanDate}.
+     */
+    public void setLoanDate(LocalDate loanDate) {
+
+        this.loanDate = loanDate;
+    }
+
+    /**
+     * @return returnDate
+     */
+    public LocalDate getReturnDate() {
+
+        return this.returnDate;
+    }
+
+    /**
+     * @param returnDate new value of {@link #getReturnDate}.
+     */
+    public void setReturnDate(LocalDate returnDate) {
+
+        this.returnDate = returnDate;
+    }
+
+    /**
+     * @return category
+     */
+    public CategoryDto getCategory() {
+
+        return this.category;
+    }
+
+    /**
+     * @param category new value of {@link #getCategory}.
+     */
+    public void setCategory(CategoryDto category) {
+
+        this.category = category;
+    }
+
+    /**
+     * @return game
+     */
+    public GameDto getGame() {
+
+        return this.game;
+    }
+
+    /**
+     * @param game new value of {@link #getGame}.
+     */
+    public void setGame(GameDto game) {
+
+        this.game = game;
+    }
+
+}
