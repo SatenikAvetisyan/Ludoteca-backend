ba096daaafd107569e64a86a21e51f426d17ad57
Avetisyan <satenik.avetisyan@capgemini.com>
2025-06-20 17:45:16 +0200

    config: añadido CorsConfig para evitar errores de conexiones con frontend

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,11 +13,11 @@ INSERT INTO author(name, nationality) VALUES ('Perepau Llistosella', 'ES');
 INSERT INTO author(name, nationality) VALUES ('Michael Kiesling', 'DE');
 INSERT INTO author(name, nationality) VALUES ('Phil Walker-Harding', 'US');
 
-INSERT INTO game(title, age, category_id, author_id) VALUES ('On Mars', '14', 1, 2);
-INSERT INTO game(title, age, category_id, author_id) VALUES ('Aventureros al tren', '8', 3, 1);
-INSERT INTO game(title, age, category_id, author_id) VALUES ('1920: Wall Street', '12', 1, 4);
+INSERT INTO game(title, age, category_id, author_id) VALUES ('On Mars', '14',1,2);
+INSERT INTO game(title, age, category_id, author_id) VALUES ('Aventureros', '8',3,1);
+INSERT INTO game(title, age, category_id, author_id) VALUES ('Wall Street', '12', 1 ,4);
 INSERT INTO game(title, age, category_id, author_id) VALUES ('Barrage', '14', 1, 3);
-INSERT INTO game(title, age, category_id, author_id) VALUES ('Los viajes de Marco Polo', '12', 1, 3);
-INSERT INTO game(title, age, category_id, author_id) VALUES ('Azul', '8', 3, 5);
+INSERT INTO game(title, age, category_id, author_id) VALUES ('Los viajes', '12', 1, 3);
+INSERT INTO game(title, age, category_id, author_id) VALUES ('Azul', '8', 3 ,5);
 
 INSERT INTO loan(game_id, client_id, loan_date, return_date) VALUES (1, 1,'2025-06-01','2025-06-10');
